2bed7667e25714653cb8a8b452e3f7fb7e1985b4
joseck ogachi <jogachi4@gmail.com>
2019-03-11 17:30:24 +0300

    add more information to urls.py

--- pictureapp/urls.py
+++ pictureapp/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns=[
     url('^$',views.user_page,name = 'user_page'),
     url(r'^search/', views.search_result, name='search_result'),
+        url(r'^user/(\d+)', views.single_user, name='single_user'),
+    url(r'^image/(\d+)', views.image, name='image'),
+    url(r'^post/', views.post, name='post'),
